3d965f4932880d70128ba8a9d950b3cc4e7dbd11
OZ2308 <ossama994@gmail.com>
2021-06-07 12:06:09 +0100

    README modifications
    Modifications to the README file for further description

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -17,10 +17,11 @@ def home(request):
     all_todo_items = todoitem.objects.all()
     return render(request, 'todo/home.html', {"all_items": all_todo_items})
 
-def about_page(request):
-    return render(request, 'todo/about.html')
-
 def deletetodo(request, todo_id):
     item_to_delete = todoitem.objects.get(id=todo_id)
     item_to_delete.delete()
     return HttpResponseRedirect("/home/")
+    
+def about_page(request):
+    return render(request, 'todo/about.html')
+
