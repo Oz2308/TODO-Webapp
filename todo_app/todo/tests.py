2dfc0e54d0ab35046deb1391ebffde4512430698
OZ2308 <ossama994@gmail.com>
2021-07-04 19:34:05 +0100

    Favicon & Tests
    Removed favicon error by inserting html in base.html. Created first couple of tests for each page

--- todo_app/todo/tests.py
+++ todo_app/todo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class test_urls(TestCase):
+    def test_welcomepage(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_homepage(self):
+        response_home = self.client.get("/home/")
+        self.assertEqual(response_home.status_code, 200)
+        
+    def test_aboutpage(self):
+        response_about = self.client.get("/about/")
+        self.assertEqual(response_about.status_code, 200)    
