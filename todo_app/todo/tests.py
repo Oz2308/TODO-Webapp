0b3a63f3dcefc12b7a4d5b49247e7756ccf3eb65
OZ2308 <ossama994@gmail.com>
2021-09-05 13:08:48 +0100

    Tests snake case
    snake case

--- todo_app/todo/tests.py
+++ todo_app/todo/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .models import ToDoItem
 
 
-class test_urls(TestCase):
+class TestUrls(TestCase):
     def test_welcomepage(self):
         response = self.client.get("")
         self.assertEqual(response.status_code, 200)
@@ -15,24 +15,24 @@ class test_urls(TestCase):
         response_about = self.client.get("/about/")
         self.assertEqual(response_about.status_code, 200)
 
-class test_crud(TestCase):
+class TestCrud(TestCase):
     def setUp(self):
         ToDoItem.objects.create(item="shopping")
 
-    def testCreatetodo(self):
+    def test_createtodo(self):
         new_item = self.client.post("/home/", {"content":"TESTITEM"})
         self.assertEqual(new_item.status_code, 302)
         todoitem = ToDoItem.objects.get(item = "TESTITEM")
         self.assertTrue(todoitem)
     
-    def testDeletetodo(self):
+    def test_deletetodo(self):
         todoitem = ToDoItem.objects.get(item = "shopping")
         todoid = todoitem.id
         deleting = self.client.post(f"/deletetodo/{todoid}/")
         with self.assertRaises(ToDoItem.DoesNotExist):
             ToDoItem.objects.get(item = "shopping")
 
-    def testUpdateTodo(self):
+    def test_updateTodo(self):
         todoitem = ToDoItem.objects.get(item = "shopping")
         todoid = todoitem.id
         updating = self.client.post(f"/updatetodo/{todoid}/", {"UpdatedItem":"WALKING"})
